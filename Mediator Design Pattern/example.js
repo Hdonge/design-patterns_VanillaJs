3973efef99def174c2cbaee50e25482af39ac80b
Hdonge <harshal.donge@gmail.com>
2021-01-04 18:51:51 +0530

    Mediator design pattern

---
+++ Mediator Design Pattern/example.js
@@ -0,0 +1,47 @@
+/**
+ * Mediator design pattern
+ */
+
+class Member {
+    constructor(name) {
+        this.name = name;
+        this.chatroom = null;
+    }
+
+    send(message, toMember) {
+        this.chatroom.send(message, this, toMember);
+    }
+
+    receive(message, fromMember) {
+        console.log(`${fromMember.name} to ${this.name}: ${message}`);
+    }
+}
+
+class Chatroom {
+    constructor() {
+        this.members = {};
+    }
+
+    addMember(member) {
+        this.members[member.name] = member;
+        member.chatroom = this;
+    }
+
+    send(message, fromMember, toMember) {
+        toMember.receive(message, fromMember);
+    }
+}
+
+
+const chat = new Chatroom();
+const bob = new Member('Bob');
+const john = new Member('John');
+const tim = new Member('Tim');
+
+chat.addMember(bob);
+chat.addMember(john);
+chat.addMember(tim);
+
+bob.send("Hey, John", john);
+john.send("whats up, bob", bob);
+tim.send("Are you okay John", john);
